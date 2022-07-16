e188a1b210aa05120ad85196ac6c9f9f4989fae4
IDDna <romanov1371@gmail.com>
2022-07-16 11:46:39 +0300

    Add warning message

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const passwordOne = document.querySelector('#pwd');
 const passwordTwo = document.querySelector('#confirm_pwd');
 const submitBtn = document.querySelector('button');
+const warning = document.querySelector('.warning');
 
 submitBtn.addEventListener('click', () => {
    if (passwordOne.value !== passwordTwo.value) {
       passwordOne.classList.add('invalid');
       passwordTwo.classList.add('invalid');
       passwordTwo.value = '';
+      warning.classList.add('active');
    }
 })
